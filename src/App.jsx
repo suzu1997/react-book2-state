1ecf1a783fbd9ff382cbdc4d99d2702e914dcd0b
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-11-23 18:22:20 +0900

    propsをバケツリレーするアプリケーションを作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import { Card } from './components/Card';
 
 function App() {
-  return <div>
+  // 管理者フラグ
+  const [isAdmin, setIsAdmin] = useState(false);
 
-  </div>;
+  const onClickSwitch = () => setIsAdmin(!isAdmin);
+
+  return (
+    <div>
+      {/* 管理者かどうかで文字を出し分け */}
+      {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
+      <button onClick={onClickSwitch}>切り替え</button>
+      <Card isAdmin={isAdmin} />
+    </div>
+  );
 }
 
 export default App;
